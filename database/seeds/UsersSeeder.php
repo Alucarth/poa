313633332f38fce8824312c6b31db50259781d60
David Torrez Salinas <ltorrezs2008@gmail.com>
2019-01-28 19:08:04 -0400

    add user role and login with username

---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'username' =>  'admin',
+            'name' =>  'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('123456'),
+        ]);
+
+        DB::table('roles')->insert([
+            'name' =>  'admin',
+            'description' => 'Administrador del Sistema',
+        ]);
+
+        DB::table('user_roles')->insert([
+            'user_id' =>  1,
+            'rol_id' =>  1,
+        ]);
+        
+    }
+}
