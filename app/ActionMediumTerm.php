12d9ad875e71e6738b08b02ef0ae258c345d7037
alucarth <ltorrezs2008@gmail.com>
2019-07-23 12:30:18 -0400

    softdelete action medium termn

--- app/ActionMediumTerm.php
+++ app/ActionMediumTerm.php
@@ -3,10 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class ActionMediumTerm extends Model
 {
     //
+    use SoftDeletes;
 
     public function years()
     {
