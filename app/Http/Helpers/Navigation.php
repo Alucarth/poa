bc64775a4a1aea952ba1c671a242255325dbf218
alucarth <ltorrezs2008@gmail.com>
2019-12-03 12:00:30 -0400

    creacion de tareas

---
+++ app/Http/Helpers/Navigation.php
@@ -0,0 +1,16 @@
+<?php
+class Navigation
+{
+    public static function isActiveRoute($route, $output = 'active')
+    {
+        if(is_array($route)) {
+            if (in_array(Route::currentRouteName(), $route)) {
+                return $output;
+            }
+        }else{
+            if (Route::currentRouteName() == $route) {
+                return $output;
+            }
+        }
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        require_once __DIR__ . '/../Http/Helpers/Util.php';
+        require_once __DIR__ . '/../Http/Helpers/Navigation.php';
     }
 
     /**
